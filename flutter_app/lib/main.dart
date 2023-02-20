34168151ab7f8b1703cc8ccaabf63cf7261381ac
kaustav991 <kaustav.wlh@gmail.com>
2023-02-20 11:51:57 +0530

    1st Upload

---
+++ flutter_app/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('EasyList'),
+        ),
+        body: Card(
+          child: Column(
+            children: <Widget>[
+              Image.asset('assets/tree-736885__480.jpg'),
+              const Text('Demo Text')
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
