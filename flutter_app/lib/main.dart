97e191eab969defb8374cec14556974c6ae5fbea
kaustav991 <kaustav.wlh@gmail.com>
2023-03-09 15:52:10 +0530

    ...

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -15,80 +15,82 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.deepOrange)
             .copyWith(secondary: Colors.deepPurple),
       ),
-      home: Scaffold(
-        drawer: SafeArea(
-          child: Drawer(
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  ListTile(
-                    tileColor: Color(0xFFFF9000),
-                    focusColor: Color(0xFFFF9000),
-                    hoverColor: Color(0xFFFF9000),
-                    title: Text('Home'),
-                    leading: Icon(Icons.home),
-                  ),
-                  ListTile(
-                    title: Text('Hot Spots'),
-                    leading: Icon(Icons.fire_extinguisher),
-                  ),
-                  Divider(thickness: 1),
-                  ListTile(
-                    title: Text('Local Files'),
-                    leading: Icon(Icons.download),
-                  ),
-                  ListTile(
-                    title: Text('Marked Files'),
-                    leading: Icon(Icons.tag),
-                  ),
-                  ListTile(
-                    title: Text('Cloud Storage'),
-                    leading: Icon(Icons.cloud),
-                  ),
-                  ListTile(
-                    title: Text('Hot Spots'),
-                    leading: Icon(Icons.fire_extinguisher),
-                  ),
-                  Divider(thickness: 1),
-                  ListTile(
-                    title: Text('Local Files'),
-                    leading: Icon(Icons.download),
-                  ),
-                  ListTile(
-                    title: Text('Marked Files'),
-                    leading: Icon(Icons.tag),
-                  ),
-                  ListTile(
-                    title: Text('Cloud Storage'),
-                    leading: Icon(Icons.cloud),
-                  ),
-                  ListTile(
-                    title: Text('Hot Spots'),
-                    leading: Icon(Icons.fire_extinguisher),
-                  ),
-                  Divider(thickness: 1),
-                  ListTile(
-                    title: Text('Local Files'),
-                    leading: Icon(Icons.download),
-                  ),
-                  ListTile(
-                    title: Text('Marked Files'),
-                    leading: Icon(Icons.tag),
-                  ),
-                  ListTile(
-                    title: Text('Cloud Storage'),
-                    leading: Icon(Icons.cloud),
-                  ),
-                ],
+      home: Material(
+        child: Scaffold(
+          drawer: SafeArea(
+            child: Drawer(
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    ListTile(
+                      tileColor: Color(0xFFFF9000),
+                      focusColor: Color(0xFFFF9000),
+                      hoverColor: Color(0xFFFF9000),
+                      title: Text('Home'),
+                      leading: Icon(Icons.home),
+                    ),
+                    ListTile(
+                      title: Text('Hot Spots'),
+                      leading: Icon(Icons.fire_extinguisher),
+                    ),
+                    Divider(thickness: 1),
+                    ListTile(
+                      title: Text('Local Files'),
+                      leading: Icon(Icons.download),
+                    ),
+                    ListTile(
+                      title: Text('Marked Files'),
+                      leading: Icon(Icons.tag),
+                    ),
+                    ListTile(
+                      title: Text('Cloud Storage'),
+                      leading: Icon(Icons.cloud),
+                    ),
+                    ListTile(
+                      title: Text('Hot Spots'),
+                      leading: Icon(Icons.fire_extinguisher),
+                    ),
+                    Divider(thickness: 1),
+                    ListTile(
+                      title: Text('Local Files'),
+                      leading: Icon(Icons.download),
+                    ),
+                    ListTile(
+                      title: Text('Marked Files'),
+                      leading: Icon(Icons.tag),
+                    ),
+                    ListTile(
+                      title: Text('Cloud Storage'),
+                      leading: Icon(Icons.cloud),
+                    ),
+                    ListTile(
+                      title: Text('Hot Spots'),
+                      leading: Icon(Icons.fire_extinguisher),
+                    ),
+                    Divider(thickness: 1),
+                    ListTile(
+                      title: Text('Local Files'),
+                      leading: Icon(Icons.download),
+                    ),
+                    ListTile(
+                      title: Text('Marked Files'),
+                      leading: Icon(Icons.tag),
+                    ),
+                    ListTile(
+                      title: Text('Cloud Storage'),
+                      leading: Icon(Icons.cloud),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
+          appBar: AppBar(
+            title: const Text('EasyList'),
+          ),
+          body: SingleChildScrollView(child: ProductManager('Food Tester')),
         ),
-        appBar: AppBar(
-          title: const Text('EasyList'),
-        ),
-        body: SingleChildScrollView(child: ProductManager('Food Tester')),
       ),
     );
   }
